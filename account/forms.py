0c7a7be49a3b060372d8ee9052a58527f0aa53cc
Stefan Marchhart <stefanmarchh@gmail.com>
2017-01-14 21:13:03 -0500

    Fixed static files

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,13 @@ from django import forms
 
 
 # Defines a login form, which allows for login
+from account.models import BoycottUser
+
+
 class LoginForm(AuthenticationForm):
     username = forms.CharField()
     password = forms.PasswordInput()
+
+# class UserForm(forms.ModelForm):
+#     class Meta:
+#         model = BoycottUser
