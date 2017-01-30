03a0865f110ffcfc638693affd03ee028a1ddac1
Stefan Marchhart <stefanmarchh@gmail.com>
2017-01-30 14:37:18 -0500

    Fixed sso

--- account/disqus.py
+++ account/disqus.py
@@ -18,23 +18,29 @@ def get_disqus_sso(user_id=None, username=None, email=None):
     else:
         user_data = {}
     # create a JSON packet of our data attributes
-    data = simplejson.dumps(user_data).encode('utf-8')
-    # encode the data to base64
-    message = base64.b64encode(data)
-    # generate a timestamp for signing the message
+    data = simplejson.dumps(user_data)
+
+    b64_data = str(base64.b64encode(data.encode('utf-8')),'utf-8')
+
     timestamp = str(int(time.time()))
+    # encode the data to base64
+    message = b64_data+' '+timestamp
+    message = message.encode()
+    # generate a timestamp for signing the message
     key=bytes(DISQUS_SECRET_KEY,"utf-8")
     # generate our hmac signature
-    # sig = hmac.HMAC(key, '%s %s' % (message, timestamp), hashlib.sha1).hexdigest()
-    sig = hmac.HMAC(key, (str(message) +" "+ timestamp).encode(), hashlib.sha1).hexdigest()
-    # sig = hmac.new(DISQUS_SECRET_KEY, (str(message) +" "+ timestamp).encode(), hashlib.sha1).hexdigest()
+
+    # sig = hmac.HMAC(key, (str(message) +" "+ timestamp).encode(), hashlib.sha1).hexdigest()
+    sig = hmac.new(key, message, hashlib.sha1).hexdigest()
+    # import ipdb; ipdb.set_trace()
+
 
 # return a script tag to insert the sso message
     return """
             this.page.remote_auth_s3 = "%(message)s %(sig)s %(timestamp)s";
         this.page.api_key = "%(pub_key)s";
     """ % dict(
-        message=message,
+        message=b64_data,
         timestamp=timestamp,
         sig=sig,
         pub_key=DISQUS_PUBLIC_KEY,
