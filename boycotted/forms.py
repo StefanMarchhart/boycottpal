971885e45e05082e1b03ba5864b9be978b4ef3c8
Stefan Marchhart <stefanmarchh@gmail.com>
2017-03-06 03:10:14 -0500

    All Boycotts:

--- boycotted/forms.py
+++ boycotted/forms.py
@@ -1,14 +1,18 @@
 from django import forms
-from boycotted.models import Boycott, Boycotted
+from boycotted.models import Boycott, Boycotted, DirtyFilterModelIFeelGuiltyAbout
 
 
 class BoycottedForm(forms.ModelForm):
     class Meta:
         model = Boycotted
-        fields = ['name', 'zip']
+        fields = ['name', 'zip', 'tag']
 
 class BoycottForm(forms.ModelForm):
     class Meta:
         model = Boycott
         fields = ['reason']
 
+class FilterForm(forms.ModelForm):
+    class Meta:
+        model = DirtyFilterModelIFeelGuiltyAbout
+        fields = ['tag','sort']
